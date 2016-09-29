b280f301c6a5ae7e0b8600c8b75e73c7cd364cf0
edroque93 <quique.lpgc@gmail.com>
2016-09-29 15:05:24 +0200

    Constructors... template..

--- src/backend/backend.hpp
+++ src/backend/backend.hpp
@@ -1,13 +1,24 @@
 #ifndef BACKEND_HPP
 #define BACKEND_HPP
 
+#include <iostream>
+
 #include <cairo/cairo-svg.h>
+#include <math.h>
 
 class backend {
-	public:
-		backend() {}
-		~backend() {}
-		void doTheThing();
+   public:
+    backend() {}
+    backend(int width, int height);
+    backend(int width, int height, int margin);
+    // something something G(N,E)
+    ~backend() {}
+    void doTheThing();
+   private:
+    const int default_margin = 32;
+    const int default_width = 512;
+    const int default_height = 512;
+    int width, height, margin;
 };
 
 #endif
