03a663a8a0a207e37da67ab326114ab3ceb47510
edroque93 <quique.lpgc@gmail.com>
2016-09-29 13:04:30 +0200

    Simple test

--- src/backend/backend.cpp
+++ src/backend/backend.cpp
@@ -1,27 +1,41 @@
+#include <math.h>
 #include <backend.hpp>
+#include <iostream>
 
 void backend::doTheThing() {
-    cairo_surface_t *surface = cairo_svg_surface_create("test.svg", 512, 512);
+    const int width = 512;
+    const int height = 512;
+    const int margin = 32;
+    const int nodes = 4;
+    const double x[] = {0., 0.33, 0.66, 1.};
+    const double y[] = {0., 0.33, 0.33, 0.};
+    const bool edges[nodes][nodes] = {
+        {0, 1, 1, 1}, {0, 0, 1, 1}, {0, 0, 0, 1}, {0, 0, 0, 0}};
+
+    cairo_surface_t *surface =
+        cairo_svg_surface_create("test.svg", width, height);
     cairo_t *cr = cairo_create(surface);
+    cairo_set_line_width(cr, 1.0);
 
-    cairo_set_line_width(cr, 40.96);
-    cairo_move_to(cr, 76.8, 84.48);
-    cairo_rel_line_to(cr, 51.2, -51.2);
-    cairo_rel_line_to(cr, 51.2, 51.2);
-    cairo_set_line_join(cr, CAIRO_LINE_JOIN_MITER); /* default */
-    cairo_stroke(cr);
-
-    cairo_move_to(cr, 76.8, 161.28);
-    cairo_rel_line_to(cr, 51.2, -51.2);
-    cairo_rel_line_to(cr, 51.2, 51.2);
-    cairo_set_line_join(cr, CAIRO_LINE_JOIN_BEVEL);
-    cairo_stroke(cr);
-
-    cairo_move_to(cr, 76.8, 238.08);
-    cairo_rel_line_to(cr, 51.2, -51.2);
-    cairo_rel_line_to(cr, 51.2, 51.2);
-    cairo_set_line_join(cr, CAIRO_LINE_JOIN_ROUND);
-    cairo_stroke(cr);
+    for (int i = 0; i < nodes; i++) {
+        int nodex = margin + (width - margin * 2) * x[i];
+        int nodey = margin + (height - margin * 2) * y[i];
+        cairo_arc(cr, nodex, nodey, 10.0, 0.0, M_PI * 2.0);
+        cairo_fill(cr);
+        cairo_stroke(cr);
+        for (int j = i; j < nodes; j++) {
+            if (edges[i][j]) {
+                cairo_move_to(cr, nodex, nodey);
+                cairo_line_to(cr, margin + (width - margin * 2) * x[j],
+                              margin + (height - margin * 2) * y[j]);
+                std::cout << "From (" << nodex << ", " << nodey << ") to ("
+                          << margin + (width - margin * 2) * x[j] << ", "
+                          << margin + (height - margin * 2) * y[j] << ")"
+                          << std::endl;
+                cairo_stroke(cr);
+            }
+        }
+    }
 
     cairo_surface_flush(surface);
     cairo_surface_destroy(surface);
