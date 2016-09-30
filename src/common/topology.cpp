b387134c57cac1c0525fb129a98551777db844fc
Calvin Bulla <calvin.bulla@gmail.com>
2016-09-30 15:29:30 +0200

    Added move/copy constructor

--- src/common/topology.cpp
+++ src/common/topology.cpp
@@ -13,89 +13,118 @@
 using namespace std;
 
 topology::~topology() {
-  if (matrix != nullptr)
-    gsl_matrix_free(matrix);
+    if (matrix != nullptr) gsl_matrix_free(matrix);
+}
+
+topology::topology(const topology &other) {
+    matrix = gsl_matrix_alloc(other.size(), other.size());
+    if (matrix == NULL) throw runtime_error("unable to allocate matrix");
+    gsl_matrix_memcpy(matrix, other.matrix);
+}
+
+topology &topology::operator=(const topology &other) {
+    if (this != &other) {
+        if (size() != other.size()) {
+            gsl_matrix_free(matrix);
+            matrix = gsl_matrix_alloc(other.size(), other.size());
+            if (matrix == NULL)
+                throw runtime_error("unable to allocate matrix");
+        }
+        gsl_matrix_memcpy(matrix, other.matrix);
+    }
+    return *this;
+}
+
+topology::topology(topology &&other) {
+    matrix = other.matrix;
+    other.matrix = nullptr;
+}
+
+topology &topology::operator=(topology &&other) {
+    if (this != &other) {
+        gsl_matrix_free(matrix);
+        matrix = other.matrix;
+        other.matrix = nullptr;
+    }
+    return *this;
 }
 
 topology topology::read_csv(const string &filename) {
-  topology top;
-  string line;
-  ifstream ifs(filename);
+    topology top;
+    string line;
+    ifstream ifs(filename);
 
-  const char *delim = ";,\t ";
+    const char *delim = ";,\t ";
 
-  if (!getline(ifs, line))
-    throw std::runtime_error("file is empty");
-  vector<double> values = str_split<double>(line, delim);
+    if (!getline(ifs, line)) throw std::runtime_error("file is empty");
+    vector<double> values = str_split<double>(line, delim);
 
-  if (values.size() == 0)
-    throw std::runtime_error("no values");
+    if (values.size() == 0) throw std::runtime_error("no values");
 
-  size_t size = values.size();
+    size_t size = values.size();
 
-  gsl_matrix *mat = gsl_matrix_alloc(size, size);
-  if (mat == NULL)
-    throw runtime_error("unable to allocate matrix");
-
-  for (size_t j = 0; j < size; ++j) {
-    gsl_matrix_set(mat, 0, j, values[j]);
-  }
-
-  for (size_t i = 1; i < size; ++i) {
-    if (!getline(ifs, line)) {
-      gsl_matrix_free(mat);
-      throw std::runtime_error("unexpected end of file");
-    }
-    values = str_split<double>(line, delim);
-    if (values.size() != size) {
-      gsl_matrix_free(mat);
-      throw std::runtime_error("wrong number of values");
-    }
+    gsl_matrix *mat = gsl_matrix_alloc(size, size);
+    if (mat == NULL) throw runtime_error("unable to allocate matrix");
 
     for (size_t j = 0; j < size; ++j) {
-      gsl_matrix_set(mat, i, j, values[j]);
+        gsl_matrix_set(mat, 0, j, values[j]);
     }
-  }
 
-  getline(ifs, line);
-  if (!ifs.eof()) {
-    gsl_matrix_free(mat);
-    throw std::runtime_error("expected end of file");
-  }
+    for (size_t i = 1; i < size; ++i) {
+        if (!getline(ifs, line)) {
+            gsl_matrix_free(mat);
+            throw std::runtime_error("unexpected end of file");
+        }
+        values = str_split<double>(line, delim);
+        if (values.size() != size) {
+            gsl_matrix_free(mat);
+            throw std::runtime_error("wrong number of values");
+        }
 
-  top.matrix = mat;
-  return top;
+        for (size_t j = 0; j < size; ++j) {
+            gsl_matrix_set(mat, i, j, values[j]);
+        }
+    }
+
+    getline(ifs, line);
+    if (!ifs.eof()) {
+        gsl_matrix_free(mat);
+        throw std::runtime_error("expected end of file");
+    }
+
+    top.matrix = mat;
+    return top;
 }
 
 void topology::save_csv(const string &filename, char delim) {
-  std::ofstream ofs(filename);
-  for (size_t i = 0; i < size(); ++i) {
-    for (size_t j = 0; j < size() - 1; ++j) {
-      ofs << gsl_matrix_get(matrix, i, j) << delim;
+    std::ofstream ofs(filename);
+    for (size_t i = 0; i < size(); ++i) {
+        for (size_t j = 0; j < size() - 1; ++j) {
+            ofs << gsl_matrix_get(matrix, i, j) << delim;
+        }
+        ofs << gsl_matrix_get(matrix, i, size() - 1) << '\n';
     }
-    ofs << gsl_matrix_get(matrix, i, size() - 1) << '\n';
-  }
 }
 
 void topology::save_dat(const string &filename) {
-  FILE *fp = fopen(filename.c_str(), "wb");
-  if (fp == NULL) {
-    throw std::runtime_error("unable to open file " + filename + ": " +
-                             strerror(errno));
-  }
-  if (gsl_matrix_fwrite(fp, matrix) != 0) {
+    FILE *fp = fopen(filename.c_str(), "wb");
+    if (fp == NULL) {
+        throw std::runtime_error("unable to open file " + filename + ": " +
+                                 strerror(errno));
+    }
+    if (gsl_matrix_fwrite(fp, matrix) != 0) {
+        fclose(fp);
+        throw std::runtime_error("unable to save matrix");
+    }
     fclose(fp);
-    throw std::runtime_error("unable to save matrix");
-  }
-  fclose(fp);
 }
 
 std::ostream &operator<<(ostream &os, const topology &top) {
-  for (size_t i = 0; i < top.size(); ++i) {
-    for (size_t j = 0; j < top.size() - 1; ++j) {
-      os << gsl_matrix_get(top.matrix, i, j) << ' ';
+    for (size_t i = 0; i < top.size(); ++i) {
+        for (size_t j = 0; j < top.size() - 1; ++j) {
+            os << gsl_matrix_get(top.matrix, i, j) << ' ';
+        }
+        os << gsl_matrix_get(top.matrix, i, top.size() - 1) << '\n';
     }
-    os << gsl_matrix_get(top.matrix, i, top.size() - 1) << '\n';
-  }
-  return os;
+    return os;
 }
--- src/common/topology.hpp
+++ src/common/topology.hpp
@@ -7,25 +7,25 @@
 #include <gsl/gsl_matrix.h>
 
 class topology {
-  gsl_matrix *matrix = nullptr;
-  topology(){};
+    gsl_matrix *matrix = nullptr;
+    topology(){};
 
-public:
-  ~topology();
+   public:
+    ~topology();
 
-  topology(const topology &);
-  topology &operator=(const topology &);
+    topology(const topology &);
+    topology &operator=(const topology &);
 
-  topology(topology &&);
-  topology &operator=(topology &&);
+    topology(topology &&);
+    topology &operator=(topology &&);
 
-  inline size_t size() const { return matrix->size1; }
+    inline size_t size() const { return matrix->size1; }
 
-  static topology read_csv(const std::string &filename);
-  static topology read_dat(const std::string &filename);
+    static topology read_csv(const std::string &filename);
+    static topology read_dat(const std::string &filename);
 
-  void save_csv(const std::string &filename, char delim = ' ');
-  void save_dat(const std::string &filename);
+    void save_csv(const std::string &filename, char delim = ' ');
+    void save_dat(const std::string &filename);
 
-  friend std::ostream &operator<<(std::ostream &os, const topology &top);
+    friend std::ostream &operator<<(std::ostream &os, const topology &top);
 };
