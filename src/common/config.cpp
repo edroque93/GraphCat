c3e51b278348055cc2f3166761837f0f1e186dd9
Calvin Bulla <calvin.bulla@gmail.com>
2016-10-03 10:54:15 +0200

    Added template for config

---
+++ src/common/config.cpp
@@ -0,0 +1,33 @@
+#include "config.hpp"
+
+using namespace std;
+
+const string &config::section::operator[](const string &) const {
+    // TODO Fill in code for section::operator[]
+    return values.begin()->second;
+}
+
+bool config::section::is_set(const string &key) const {
+    // TODO Fill in code for section::is_set
+    return false;
+}
+
+config config::read_ini(const string &filename) {
+    // TODO Fill in code for config::read_ini
+    config cfg;
+    return move(cfg);
+}
+
+void config::save_ini(const string &filename) {
+    // TODO Fill in code for config::save_ini
+}
+
+bool config::has_section(const string &) const {
+    // TODO Fill in code for config::has_section
+    return false; }
+
+config::section config::operator[](const string &name) const {
+    // TODO Fill in code for config::operator[]
+    config::section sec(name, values.begin()->second);
+    return move(sec);
+}
---
+++ src/common/config.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <exception>
+#include <map>
+#include <string>
+
+#include "../utils/stringops.hpp"
+
+// TODO does config need to be 'immutable'?
+
+class config {
+    std::map<std::string, std::map<std::string, std::string> > values;
+
+   public:
+    class section {
+        const std::string &name;
+        const std::map<std::string, std::string> &values;
+
+       public:
+        section(const std::string &n,
+                const std::map<std::string, std::string> &v)
+            : name(n), values(v) {}
+
+        const std::string &operator[](const std::string &) const;
+        bool is_set(const std::string &key) const;
+
+        template <class T>
+        T get(const std::string &key) const {
+            if (!is_set(key)) throw std::runtime_error(key + " not set");
+            return str_as<T>((*this)[key]);
+        }
+
+        template <class T>
+        T get(const std::string &key, T def) const {
+            return is_set(key) ? str_as<T>((*this)[key]) : def;
+        }
+    };
+
+    static config read_ini(const std::string &filename);
+    void save_ini(const std::string &filename);
+
+    bool has_section(const std::string &) const;
+    section operator[](const std::string &) const;
+};
