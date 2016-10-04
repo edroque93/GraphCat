bc1e3d329005ef332a9745dd2913ac6ef96a0313
edroque93 <quique.lpgc@gmail.com>
2016-10-04 14:25:42 +0200

    Drawing directed/undirected graphs

--- src/backend/backend.cpp
+++ src/backend/backend.cpp
@@ -2,87 +2,51 @@
 
 using namespace std;
 
-template <>
-backend::img_format str_as(const std::string &str) {
-    if (str == "svg") return backend::img_format::SVG;
-    if (str == "png") return backend::img_format::PNG;
-    throw runtime_error("unknown image format " + str);
-}
-
 void backend::plot(const database &format) {
-    for (size_t i = 0; i < format.rows(); ++i) {
-        int width, height, margin;
-        string filename;
-        img_format ext;
-        auto rec = format[i];
-        width = rec.get<int>("width", default_width);
-        height = rec.get<int>("height", default_height);
-        margin = rec.get<int>("margin", default_margin);
-        filename = rec.get<string>("filename", "plot_" + to_string(i));
-        ext = rec.get<img_format>("format", default_format);
-        do_plot(width, height, margin, filename, ext);
-    }
+    // TODO Use config
+    auto rec = format[0];
+    int width = rec.get<int>("width", default_width);
+    int height = rec.get<int>("height", default_width);
+    int margin = rec.get<int>("margin", default_margin);
+    cairo = new canvas(rec.get<string>("filename", "plot"),
+                       rec.get<canvas::img_format>("format", default_format),
+                       width, height);
+    do_plot(width, height, margin);
 }
 
-void backend::do_plot(int width, int height, int margin, const string &filename,
-                      img_format ext) {
-    cairo_surface_t *surface;
-    switch (ext) {
-        case PNG:
-            surface =
-                cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
-            break;
-        case SVG:
-        default:
-            string name = filename + ".svg";
-            surface = cairo_svg_surface_create(name.c_str(), width, height);
-    }
-    cairo_t *cr = cairo_create(surface);
-    cairo_set_line_width(cr, 1.0);
+void backend::do_plot(int width, int height, int margin) {
+    // TODO Apply attributes
+    cairo->rgba(0.86, 0.77, 0.87, 1.0);
+    cairo->draw_rectangle(0, 0, width, height);
+    cairo->watermark();
     const double node_radius = 7.0;
     for (size_t i = 0; i < topo.size(); ++i) {
+        cairo->rgba(vx[i], vx[i], vx[i], 1);
         int nodex = margin + (width - margin * 2) * vx[i];
         int nodey = margin + (height - margin * 2) * vy[i];
-        cairo_arc(cr, nodex, nodey, node_radius, 0.0, M_PI * 2.0);
-        cairo_set_source_rgba(cr, 0, 0, 1.0, 1.0);
-        cairo_fill(cr);
-        cairo_stroke(cr);
+        cairo->draw_point(nodex, nodey, node_radius);
         for (size_t j = i; j < topo.size(); ++j) {
-            if (topo.get(i, j)) {
+            if (topo.get(i, j) && topo.get(j, i)) {
                 int nodex_end = margin + (width - margin * 2) * vx[j];
                 int nodey_end = margin + (height - margin * 2) * vy[j];
-                cairo_move_to(cr, nodex, nodey);
-                cairo_line_to(cr, nodex_end, nodey_end);
-                cairo_stroke(cr);
-
-                draw_arrow(cr, nodex, nodey, nodex_end, nodey_end, node_radius);
+                cairo->draw_line(nodex, nodey, nodex_end, nodey_end);
+                continue;
+            }
+            if (topo.get(i, j) && !topo.get(j, i)) {
+                int nodex_end = margin + (width - margin * 2) * vx[j];
+                int nodey_end = margin + (height - margin * 2) * vy[j];
+                cairo->draw_arrow(nodex, nodey, nodex_end, nodey_end,
+                                  node_radius);
+                continue;
+            }
+            if (!topo.get(i, j) && topo.get(j, i)) {
+                int nodex_end = margin + (width - margin * 2) * vx[j];
+                int nodey_end = margin + (height - margin * 2) * vy[j];
+                cairo->draw_arrow(nodex_end, nodey_end, nodex, nodey,
+                                  node_radius);
+                continue;
             }
         }
     }
-
-    if (ext == PNG) {
-        string name = filename + ".png";
-        cairo_surface_write_to_png(surface, name.c_str());
-    }
-
-    cairo_surface_flush(surface);
-    cairo_surface_destroy(surface);
-    cairo_destroy(cr);
-}
-
-void backend::draw_arrow(cairo_t *cr, double x1, double y1, double x2,
-                         double y2, double offset) {
-    const double arrow_angle = 30 * (M_PI / 180);
-    const double arrow_side = 10.0;
-    double arctan = atan2(y2 - y1, x2 - x1) + M_PI;
-    x2 += cos(arctan) * offset;
-    y2 += sin(arctan) * offset;
-    double head_x1 = x2 + arrow_side * cos(arctan - arrow_angle);
-    double head_y1 = y2 + arrow_side * sin(arctan - arrow_angle);
-    double head_x2 = x2 + arrow_side * cos(arctan + arrow_angle);
-    double head_y2 = y2 + arrow_side * sin(arctan + arrow_angle);
-    cairo_move_to(cr, head_x1, head_y1);
-    cairo_line_to(cr, x2, y2);
-    cairo_line_to(cr, head_x2, head_y2);
-    cairo_fill(cr);
+    cairo->flush();
 }
--- src/backend/backend.hpp
+++ src/backend/backend.hpp
@@ -2,42 +2,36 @@
 #define BACKEND_HPP
 
 #include <iostream>
-#include <map>
 #include <stdexcept>
 #include <vector>
 
 #include <cairo/cairo-svg.h>
-#include <math.h>
 
 #include "../common/database.hpp"
 #include "../common/topology.hpp"
 #include "../utils/stringops.hpp"
+#include "canvas.hpp"
 
 class backend {
    public:
-    enum img_format { SVG, PNG };
-
     backend(const topology &topo, const std::vector<double> &vx,
             const std::vector<double> &vy, const database &attrib)
         : topo(topo), vx(vx), vy(vy), attrib(attrib){};
-    ~backend() {}
+    ~backend() { delete cairo; }
     void plot(const database &format);
-    void doTheThing();
 
    private:
     const int default_margin = 32;
     const int default_width = 512;
     const int default_height = 512;
-    const img_format default_format = SVG;
+    const canvas::img_format default_format = canvas::img_format::SVG;
 
     topology topo;
     std::vector<double> vx, vy;
     database attrib;
+    canvas *cairo;
 
-    void do_plot(int width, int height, int margin, const std::string &filename,
-                 img_format ext);
-    void draw_arrow(cairo_t *cr, double x1, double y1, double x2, double y2,
-                    double offset);
+    void do_plot(int width, int height, int margin);
 };
 
 #endif
