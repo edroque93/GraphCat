de9df52e624a16ffbea0ef1719c1547574e34f3e
Calvin Bulla <calvin.bulla@gmail.com>
2016-10-06 14:35:37 +0200

    autocomplete autogenerates

--- tools/compl.sh
+++ tools/compl.sh
@@ -1,4 +1,25 @@
-gcat_opts="-help -verbose -filters -topo -save-csv -save-dat"
+gcat_opts=$(awk "
+/option_list/ {
+flag=1;
+next;
+}
+
+/};/ {
+flag=0;
+}
+
+flag {
+a=index(\$0, \"{\")+2;
+b=index(\$0, \",\")-1;
+print \"-\"substr(\$0, a, b-a);
+}
+
+END {
+print \"-help\";
+}
+" src/main.cpp)
+
+# gcat_opts="-help -verbose -filters -topo -save-csv -save-dat"
 
 _graphcat() {
     local cur="${COMP_WORDS[COMP_CWORD]}"
