fc628c7d407b39a6e2ed028168131b44923d60c2
Calvin Bulla <calvin.bulla@gmail.com>
2017-01-18 17:32:00 +0100

    modified matrix script & default config

--- tools/matrix.py
+++ tools/matrix.py
@@ -3,25 +3,29 @@
 import sys
 import numpy
 
-if len(sys.argv) != 2:
-	print "Missing argument!"
-	sys.exit()
+if len(sys.argv) < 2:
+    print "Missing argument!"
+    sys.exit()
 
 size = int(sys.argv[1])
-print size
-m = numpy.random.randint(2, size=(size,size))
+prob = float(sys.argv[2]) if len(sys.argv) > 2 else 0.1
+# print size
+m = numpy.random.ranf(size=(size,size))
+
+for i in range(0,size):
+    for j in range(0,size):
+        m[i][j] = 1 if m[i][j] < prob else 0
 
 m[0][0] = 0
 for i in range(1,size):
-	for j in range(0,i):
-		if i == j: continue
-		m[j][i] = m[i][j]
-	m[i][i] = 0
+    for j in range(0,i):
+        if i == j: continue
+        m[j][i] = m[i][j]
+    m[i][i] = 0
 
 # Output
 
 for i in range(0,size):
-	for j in range(0,size):
-		print m[i][j],
-
-	print
+    for j in range(0,size):
+        print int(m[i][j]),
+    print
